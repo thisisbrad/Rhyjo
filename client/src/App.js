76a8287c3b8d227d62ea2b58c91f4b8e1dce1460
Brad Beltowski <bradbeltowski@gmail.com>
2017-12-08 01:43:52 -0500

    Got basic function to fire off object to node and saved it in MongoDB 😎

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    axios.post('/list', { title: this.title.value });
+    this.form.reset();
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +20,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <form onSubmit={this.handleSubmit} ref={form => (this.form = form)}>
+          <div className="input-field">
+            <label htmlFor="list-title">Label</label>
+            <input
+              ref={input => (this.title = input)}
+              id="list-title"
+              placeholder="title"
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
