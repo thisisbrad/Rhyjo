a4bcbfe5f97604d74197a68e9a3302f5cf30793c
Brad Beltowski <bradbeltowski@gmail.com>
2017-12-07 20:52:55 -0500

    Added in body parser

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const { json } = require('body-parser');
+
 const app = express();
+app.use(json());
+
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send({ hello: 'We in dis' });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(5000);
+const server = app.listen(PORT, () => {
+  console.log(`### Server is listening on PORT: ${server.address().port} ###`);
+});
